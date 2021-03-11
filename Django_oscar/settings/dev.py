b8f389318afd7a8b3bf16a484d34674d14246c65
bpmaurya <realvedprakash@gmail.com>
2021-03-11 16:56:12 +0530

    add

--- Django_oscar/settings/dev.py
+++ Django_oscar/settings/dev.py
@@ -7,10 +7,21 @@ DEBUG = True
 WSGI_APPLICATION = 'Django_oscar.wsgi.dev.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1v49r4m8vrl1t',
+        'USER': 'xoosusnhglapwq',
+        'PASSWORD': 'db_password', 
+        'HOST': 'ec2-54-161-239-198.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
