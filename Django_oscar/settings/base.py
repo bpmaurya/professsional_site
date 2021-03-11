8394e833406cc4ce9f0730d8b99d2e6e1027bfbd
bpmaurya <realvedprakash@gmail.com>
2021-03-11 17:35:27 +0530

    add

--- Django_oscar/settings/base.py
+++ Django_oscar/settings/base.py
@@ -173,7 +173,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
+STATIC_URL = STATIC_HOST + '/static/'
+
+
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles',
