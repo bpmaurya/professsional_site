b7838bc0e72cc16a18ac60b37ff3b02cbd5c81ef
bpmaurya <realvedprakash@gmail.com>
2021-03-11 14:04:16 +0530

    add

--- Django_oscar/settings/prod.py
+++ Django_oscar/settings/prod.py
@@ -11,7 +11,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_name',
         'USER': 'db_user',
-        'PASSWORD': 'db_password',
+        'PASSWORD': 'db_password', 
         'HOST': 'localhost',
         'PORT': '',
     }
