c93b26c02ef1416a23ef703eb0356b503af2cc2a
bpmaurya <realvedprakash@gmail.com>
2021-03-11 15:03:40 +0530

    add

--- Django_oscar/settings/base.py
+++ Django_oscar/settings/base.py
@@ -178,3 +178,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DISABLE_COLLECTSTATIC=1
